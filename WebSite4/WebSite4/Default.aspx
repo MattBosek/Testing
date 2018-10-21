b18969db3011bfebd855af19fb624c25492997ec
MattBosek <bosekmx@dukes.jmu.edu>
2018-10-21 14:31:19 -0400

    yes

---
+++ WebSite4/WebSite4/Default.aspx
@@ -0,0 +1,117 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        
+    </style>
+</head>
+<body style="height: 734px; width: 803px;">
+    <form id="form1" runat="server">
+        <asp:Button ID="insertB" runat="server" Text="Insert" OnClick="InsertB_Click" />
+        <asp:Button ID="commitB" runat="server" Text="Commit" OnClick="CommitB_Click" />
+        <asp:Button ID="clearB" runat="server" Text="Clear" OnClick="ClearB_Click" />
+        <asp:Button ID="popB" runat="server" Text="Populate" OnClick="PopB_Click" />
+        <asp:Button ID="exitB" runat="server" Text="Exit" OnClick="ExitB_Click" />
+        <fieldset id="personName">
+            <legend>Employee Name</legend>
+            <asp:Label ID="FirstName" runat="server" Text="First Name"></asp:Label>       
+            <br />     
+            <asp:TextBox ID="fNameText" runat="server" MaxLength="25"></asp:TextBox> 
+            <asp:TextBox ID="TextBox1" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+            <br />
+            
+            <asp:Label ID="LastName" runat="server" Text="Last Name"></asp:Label><br />
+            <asp:TextBox ID="lNameText" runat="server" MaxLength="30"></asp:TextBox>
+            <asp:TextBox ID="TextBox2" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+            <br />
+            <asp:Label ID="MiddleL" runat="server" Text="Middle Name"></asp:Label><br />
+            <asp:TextBox ID="mNameText" runat="server" MaxLength="30"></asp:TextBox>
+        </fieldset>
+        <fieldset id="address">
+            <legend>Address</legend>
+            <asp:Label ID="houseL" runat="server" Text="House Number"></asp:Label><br />
+            <asp:TextBox ID="houseNumText" runat="server" MaxLength="10"></asp:TextBox>
+            <br />
+            <asp:Label ID="streetL" runat="server" Text="Street"></asp:Label><br />
+            <asp:TextBox ID="streetText" runat="server" MaxLength="20"></asp:TextBox>
+            <asp:TextBox ID="TextBox4" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+            <br />
+            <asp:Label ID="cityL" runat="server" Text="City/County"></asp:Label><br />
+            <asp:TextBox ID="cityCoText" runat="server" MaxLength="30"></asp:TextBox>
+            <asp:TextBox ID="TextBox5" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+            <br />
+            <asp:Label ID="countryL" runat="server" Text="Country Abbr."></asp:Label><br />
+            <asp:TextBox ID="countryAbbText" runat="server" MaxLength="35"></asp:TextBox>
+            <asp:TextBox ID="TextBox7" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>  
+            <br />
+            <asp:Label ID="stateL" runat="server" Text="State Abbr."></asp:Label><br />
+            <asp:DropDownList ID="DropDownList1" runat="server">
+            </asp:DropDownList>
+            <asp:TextBox ID="TextBox6" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label7" runat="server" Text="Zip Code"></asp:Label><br />
+            <asp:TextBox ID="zipText" runat="server" MaxLength="9"></asp:TextBox>
+            <asp:TextBox ID="TextBox8" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+        </fieldset>
+        <fieldset id="employee">
+            <legend>Employment Information</legend>
+
+            <asp:Label ID="dobL" runat="server" Text="Date of Birth (MM/DD/YYYY)"></asp:Label><br />
+
+            <asp:TextBox ID="dobText" runat="server" MaxLength="10"></asp:TextBox>
+            <asp:TextBox ID="TextBox9" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+            <br />
+
+            <asp:Label ID="Label8" runat="server" Text="Hire Date (MM/DD/YYYY)"></asp:Label>
+            <br />
+            <asp:TextBox ID="hireDateText" runat="server" MaxLength="10"></asp:TextBox>
+            <asp:TextBox ID="TextBox10" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label10" runat="server" Text="Termination Date (MM/DD/YYYY)"></asp:Label><br />
+            <asp:TextBox ID="termDateText" runat="server" MaxLength="10"></asp:TextBox>
+            <asp:TextBox ID="TextBox11" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+            <br />
+            
+            <asp:Label ID="hoursL" runat="server" Text="Yearly Hours"></asp:Label><br />
+            <asp:TextBox ID="yrHourText" runat="server" MaxLength="4"></asp:TextBox>
+            <asp:TextBox ID="TextBox12" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label12" runat="server" Text="Valid Permit?"></asp:Label> <br />
+            <asp:RadioButton ID="ValidPermitY" runat="server" Text="Yes" ValidationGroup="ValidPermit" />
+            <asp:RadioButton ID="ValidPermitN" runat="server" Text="No" ValidationGroup="ValidPermit" />
+            <br />
+
+            <asp:Label ID="vaccL" runat="server" Text="Are you Rabies Vaccinated?"></asp:Label><br />
+            <asp:RadioButton ID="vacY" runat="server" GroupName="isVaccinated" Text="Yes" />
+            <asp:RadioButton ID="vacN" runat="server" GroupName="isVaccinated" Text="No" />
+            <asp:TextBox ID="TextBox13" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+            <br />
+
+            <asp:Label ID="ageL" runat="server" Text="Age"></asp:Label>
+            <asp:TextBox ID="ageText" runat="server" Height="16px" Width="22px"></asp:TextBox>
+            <br />
+
+            <asp:Label ID="Label11" runat="server" Text="Skill"></asp:Label><br />
+            <asp:DropDownList ID="DropDownList2" runat="server"></asp:DropDownList>
+            <asp:TextBox ID="TextBox14" runat="server" BorderStyle="None" ForeColor="Red"></asp:TextBox>
+            <br />
+        </fieldset>
+        <br />
+
+        <asp:TextBox ID="uneditableText" runat="server"></asp:TextBox><br />
+
+        <asp:TextBox ID="pTest1" runat="server"></asp:TextBox> <br />
+        <asp:TextBox ID="pTest2" runat="server"></asp:TextBox> <br />
+        <asp:TextBox ID="pTest3" runat="server"></asp:TextBox> <br />
+        <asp:TextBox ID="errorText" runat="server"></asp:TextBox>
+        <asp:TextBox ID="indexSize" runat="server"></asp:TextBox>
+
+
+    </form>
+    
+</body>
+</html>
---
+++ WebSite4/WebSite4/Default.aspx.cs
@@ -0,0 +1,501 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        /*try
+        {
+            System.Data.SqlClient.SqlConnection sc = new System.Data.SqlClient.SqlConnection();
+            sc.ConnectionString = @"Server =Localhost;Database=Lab1;Trusted_Connection=Yes;";
+            //Open the sql connection
+            sc.Open();
+
+            //Creates a new sql insert command to insert the data from the arrays into the Person Tabl
+            System.Data.SqlClient.SqlCommand insert = new System.Data.SqlClient.SqlCommand();
+            insert.Connection = sc;
+            for (int i = 0; i < index; i++)
+            {
+                insert.Comm
+            }
+
+        }
+        catch (Exception)
+        {
+            //resultmessage.Text = "Error Clearing Database.";
+        }
+
+        //Open the sql connection
+        sc.Open();
+
+        */
+    }
+
+    public class Person
+    {
+        //make this private later after getters and setters are made
+        public static int index = 0;
+        private string fName;
+        private string lName;
+        private string mName;
+
+        private string houseNum;
+        private string street;
+        private string cityCo;
+        private string stateAbb;
+        private string CountryAbb;
+        private string zip;
+
+        private DateTime dofBirth;
+        private DateTime hireDate;
+        private DateTime termDate;
+
+        private Boolean validPermit;
+        private string rabiesVaccinated;
+        public  int age;
+        private double yrHour;
+        
+
+
+        //also make this private after getters and setters are made
+        public static Person[] pArr = new Person[3];
+
+        public Person(string nameF, string nameL, string nameM, string houseN,
+                      string st, string city, string state, string country, string z,
+                      DateTime dOB, DateTime hDate, DateTime tDate, Boolean vac,Boolean perm,
+                      int age, double yrHour)
+        {
+            SetFName(nameF);
+            SetLName(nameL);
+            SetMName(nameM);
+
+            SetHouseNum(houseN);
+            SetStreet(st);
+            SetCityCo(city);
+            SetStateAbb(state);
+            SetCountryAbb(country);
+            SetZip(z);
+
+            SetdofBirth(dOB);
+            SetHireDate(hDate);
+            SetTermDate(tDate);
+
+            //make a method that validates vac for now, just use an if statement
+            if (vac == true)
+            {
+                SetRabiesVaccinated("Y");
+            } else
+            {
+                SetRabiesVaccinated("N");
+            }
+
+            SetValidPermit(perm);
+            SetAge(age);
+            SetYrHour(yrHour);
+        }
+
+        public void SetFName(string a)
+        {
+            this.fName = a;
+        }
+
+        public string GetFName()
+        {
+            return this.fName;
+        }
+
+        public void SetLName(string a)
+        {
+            this.lName = a;
+        }
+
+        public string GetLName()
+        {
+            return this.lName;
+        }
+
+        public void SetMName(string a)
+        {
+            this.mName = a;
+        }
+
+        public string GetMName()
+        {
+            return this.mName;
+        }
+
+        public void SetHouseNum(string a)
+        {
+            this.houseNum = a;
+        }
+
+        public string GetHouseNum()
+        {
+            return this.houseNum;
+        }
+
+        public void SetStreet(string a)
+        {
+            this.street = a;
+        }
+
+        public string GetStreet()
+        {
+            return this.street;
+        }
+
+        public void SetCityCo(string a)
+        {
+            this.cityCo = a;
+        }
+
+        public string GetCityCo()
+        {
+            return this.cityCo;
+        }
+
+        public void SetStateAbb(string a)
+        {
+            this.stateAbb = a;
+        }
+
+        public string GetStateABB()
+        {
+            return this.stateAbb;
+        }
+
+        public void SetCountryAbb(string a)
+        {
+            this.CountryAbb = a;
+        }
+
+        public string GetCountryAbb()
+        {
+            return this.CountryAbb;
+        }
+
+        public void SetZip(string a)
+        {
+            this.zip = a;
+        }
+
+        public string GetZip()
+        {
+            return this.zip;
+        }
+
+        public void SetdofBirth(DateTime a)
+        {
+            this.dofBirth = a;
+        }
+
+        public DateTime GetdofBirth()
+        {
+            return this.dofBirth;
+        }
+
+        public void SetHireDate(DateTime a)
+        {
+            this.hireDate = a;
+        }
+
+        public DateTime GetHireDate()
+        {
+            return this.hireDate;
+        }
+
+        public void SetTermDate(DateTime a)
+        {
+            this.termDate = a;
+        }
+
+        public DateTime GetTermDate()
+        {
+            return this.termDate;
+        }
+
+        public void SetRabiesVaccinated(string a)
+        {
+            this.rabiesVaccinated = a;
+        }
+
+        public string GetRabiesVaccinated()
+        {
+            return this.rabiesVaccinated;
+        }
+
+        public void SetAge(int a)
+        {
+            this.age = a;
+        }
+
+        public int GetAge()
+        {
+            return this.age;
+        }
+
+        public void SetYrHour(double a)
+        {
+            this.yrHour = a;
+        }
+
+        public double GetYrHour()
+        {
+            return this.yrHour;
+        }
+
+        public void SetValidPermit(Boolean a)
+        {
+            this.validPermit = a;
+        }
+
+        public Boolean GetValidPermit()
+        {
+            return this.validPermit;
+        }
+    }
+
+    protected void InsertB_Click(object sender, EventArgs e)
+    {
+        indexSize.Text = "" + Person.index;
+        
+        if (!IsFormValid())
+        {
+            errorText.Text = "Form is incomplete";
+        }
+        else
+        {
+
+            if (Person.index > 2)
+            {
+                errorText.Text = "Array Size Exceeded";
+            }
+            else
+            {
+
+                if (IfNameExists())
+                {
+                    errorText.Text = "Name Already Exists";
+                }
+                else
+                {
+                    //if (validateDates())
+                    //{
+                        Person current = new Person(fNameText.Text, lNameText.Text, mNameText.Text,
+                                            houseNumText.Text, streetText.Text, cityCoText.Text,
+                                            "null", countryAbbText.Text, zipText.Text,
+                                            DateTime.Parse(dobText.Text), DateTime.Parse(hireDateText.Text),
+                                            DateTime.Parse(termDateText.Text), vacY.Checked, ValidPermitY.Checked, int.Parse(ageText.Text), double.Parse(yrHourText.Text));
+                        Person.pArr[Person.index] = current;
+                        Person.index++;
+                        uneditableText.Text = "" + current.age;
+                        //testing
+
+                        pTest1.Text = "" + Person.pArr[0].GetFName() + " " + Person.pArr[0].GetLName() + " " + Person.pArr[0].GetMName() + "\n" + Person.pArr[0].GetHouseNum() + "\n" + Person.pArr[0].GetStreet() + "\n" + Person.pArr[0].GetCityCo() + "\n"
+                        + Person.pArr[0].GetCountryAbb() + "\n";
+                        if (Person.pArr[1] == null) { }
+                        else
+                        {
+                            pTest2.Text = "" + Person.pArr[1].GetFName() + " " + Person.pArr[1].GetLName() + " " + Person.pArr[1].GetMName() + "\n" + Person.pArr[1].GetHouseNum() + "\n" + Person.pArr[1].GetStreet() + "\n" + Person.pArr[1].GetCityCo() + "\n"
+                        + Person.pArr[1].GetCountryAbb() + "\n";
+                        }
+                        if (Person.pArr[2] == null) { }
+                        else
+                        {
+                            pTest3.Text = "" + Person.pArr[2].GetFName() + " " + Person.pArr[2].GetLName() + " " + Person.pArr[2].GetMName() + "\n" + Person.pArr[2].GetHouseNum() + "\n" + Person.pArr[2].GetStreet() + "\n" + Person.pArr[2].GetCityCo() + "\n"
+                            + Person.pArr[2].GetCountryAbb() + "\n";
+                        }
+                    //}
+                    
+                }
+            }
+        }
+    }
+
+    protected void CommitB_Click(object sender, EventArgs e)
+    {
+        ClearFields();
+    }
+
+    protected void ClearB_Click(object sender, EventArgs e)
+    {
+        ClearFields();
+    }
+
+    protected void PopB_Click(object sender, EventArgs e)
+    {
+        fNameText.Text = "Matt";
+        lNameText.Text = "Bosek";
+        mNameText.Text = "None";
+        houseNumText.Text = "333";
+        streetText.Text = "Univeristy Boulevard";
+        cityCoText.Text = "Leesburg";
+        countryAbbText.Text = "US";
+        zipText.Text = "22801";
+        dobText.Text = "05/02/1995";
+        hireDateText.Text = "08/17/2018";
+        termDateText.Text = "08/18/2018";
+        yrHourText.Text = "2500";
+        vacN.Checked = true;
+        ValidPermitY.Checked = true;
+    }
+
+    protected void ExitB_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    public Boolean IsFormValid()
+    {   
+        return IsFNameValid() && IsLNameValid();
+    }
+
+    public Boolean IsFNameValid()
+    {
+        Boolean result = true;
+        if (fNameText.Text.Equals("") || fNameText.Text.Equals(" "))
+        {
+            TextBox1.Text = "*";
+            result = false;
+        } else
+        {
+            TextBox1.Text = "";
+        }
+        return result;
+    }
+
+    public Boolean IsLNameValid()
+    {
+        Boolean result = true;
+        if (lNameText.Text.Equals("") || lNameText.Text.Equals(" "))
+        {
+            TextBox2.Text = "*";
+            result = false;
+        } else
+        {
+            TextBox2.Text = "";
+        }
+        return result;
+    }
+
+    public Boolean IsStreetValid()
+    {
+        Boolean result = true;
+        if (streetText.Text.Equals("") || streetText.Text.Equals(" "))
+        {
+            TextBox4.Text = "*";
+            result = false;
+        } else
+        {
+            TextBox4.Text = "";
+        }
+        return result;
+    }
+    //not completed
+    public Boolean IsDOBValid()
+    {
+        Boolean result = true;
+        return result;
+        
+    }
+
+    public Boolean IfNameExists()
+    {
+        Boolean result = false;
+        //loop through insert array
+        for (int i = 0; i < Person.pArr.Length; i++)
+        {
+            //check if reference exists
+            if (Person.pArr[i] != null)
+            {
+
+                //matches what is in text box with indexed Person
+                //If First name, last name, and middle name match, return
+                //true
+                if ((Person.pArr[i].GetFName() == fNameText.Text)
+                     && (Person.pArr[i].GetLName() == lNameText.Text)
+                     && (Person.pArr[i].GetMName() == mNameText.Text))
+                {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    public Boolean ValidateDates()
+    {
+        return ValidateDob() && ValidateHireDate() && ValidateTermDate();
+
+    }
+
+    public Boolean ValidateDob()
+    {
+        Boolean result = true;
+        DateTime tester;
+        if (!DateTime.TryParse(dobText.Text, out tester))
+        {
+            errorText.Text = "invalid date";
+            TextBox9.Text = "*";
+            result = false;
+        }
+        return result;
+    }
+
+    public Boolean ValidateHireDate()
+    {
+        Boolean result = true;
+        DateTime tester;
+        if (!DateTime.TryParse(hireDateText.Text, out tester))
+        {
+            errorText.Text = "invalid date";
+            TextBox10.Text = "*";
+            result = false;
+        }
+        return result;
+    }
+
+    public Boolean ValidateTermDate()
+    {
+        Boolean result = true;
+        DateTime tester;
+        if (!String.IsNullOrEmpty(termDateText.Text))
+        {
+            if (!DateTime.TryParse(termDateText.Text, out tester))
+            {
+                errorText.Text = "invalid date";
+                TextBox11.Text = "*";
+                result = false;
+            }
+        }
+        
+        return result;
+    }
+
+    public void ClearFields()
+    {
+        fNameText.Text = "";
+        lNameText.Text = "";
+        mNameText.Text = "";
+        houseNumText.Text = "";
+        streetText.Text = "";
+        cityCoText.Text = "";
+        countryAbbText.Text = "";
+        //state abb
+        zipText.Text = "";
+        dobText.Text = "";
+        hireDateText.Text = "";
+        termDateText.Text = "";
+        ValidPermitY.Checked = false;
+        ValidPermitN.Checked = false;
+        vacY.Checked = false;
+        vacN.Checked = false;
+        ageText.Text = ""; 
+    }
+}
